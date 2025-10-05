75e21b3e3690dc9ae08aa406ecb2c7139dab23b3
sasirasys <131147291+Sasirasys@users.noreply.github.com>
2025-10-05 23:09:19 +0700

    feat: sign in with google + home page

---
+++ auth.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import prisma from "@/prisma/prisma";
+import GoogleProvider from "next-auth/providers/google";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      console.log(
+        "user:" + user + "\naccount:" + account + "\nprofile:" + profile
+      );
+      if (!user.email || !user.name || !user.image) {
+        return false;
+      }
+
+      // Check if user exists
+      const existingUser = await prisma.user.findUnique({
+        where: { email: user.email },
+      });
+
+      if (!existingUser) {
+        // Create new user if doesn't exist
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            name: user.name,
+            img_url: user.image,
+          },
+        });
+      }
+      return true;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+});
