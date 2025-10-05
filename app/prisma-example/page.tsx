437cb4c94f5d80b424a09a5eb5d36bcc74042b37
vinitian <vinitajwp@gmail.com>
2025-10-05 16:57:48 +0700

    feat: add GET example

--- app/prisma-example/page.tsx
+++ app/prisma-example/page.tsx
@@ -3,7 +3,9 @@
 import createRoom from "@/services/client/createRoom";
 import { socket } from "@/socket";
 import { Room } from "../generated/prisma";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import getUsers from "@/services/client/getUsers";
+import { User } from "@/interface";
 
 // example for using prisma & REST API
 // things to write:
@@ -18,6 +20,15 @@ export default function PrismaExample() {
   const [username, setUsername] = useState("john");
   const [roomDetails, setRoomDetails] = useState<Room>();
   const [error, setError] = useState();
+  const [users, setUsers] = useState<User[]>();
+
+  const fetchData = async () => {
+    setUsers(await getUsers());
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const handleCreateRoom = async () => {
     const response = await createRoom({
@@ -32,18 +43,28 @@ export default function PrismaExample() {
   };
   return (
     <div>
-      <h1>This page is an example of using prisma & REST API.</h1>
+      <h1 className="text-title">
+        This page is an example of using prisma & REST API.
+      </h1>
+      <h2 className="text-subtitle">creating room</h2>
       <p>my socket id: {socket.id}</p>
-      <input
-        type="text"
-        name="username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        className="input border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-b-orange"
-      />
+      <label>
+        your username
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="input border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-b-orange"
+        />
+      </label>
+
       <button onClick={handleCreateRoom}>Create new room!</button>
       {error && <p className="text-red-400">{error}</p>}
       {roomDetails && <p>{JSON.stringify(roomDetails)}</p>}
+
+      <h2 className="text-subtitle">users signed in with google</h2>
+      <p>{JSON.stringify(users)}</p>
     </div>
   );
 }
