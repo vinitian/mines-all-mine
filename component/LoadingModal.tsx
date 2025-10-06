08ae986fd4264e1afebc9787ff90e6551799c559
vinitian <vinitajwp@gmail.com>
2025-10-06 19:05:01 +0700

    feat: add loading modal

---
+++ component/LoadingModal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
+
+export default function LoadingModal({ text = "Loading" }: { text: string }) {
+  const [loadingText, setLoadingText] = useState(`${text}.`);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (loadingText == `${text}.`) {
+        setLoadingText(`${text}..`);
+      } else if (loadingText == `${text}..`) {
+        setLoadingText(`${text}...`);
+      } else {
+        setLoadingText(`${text}.`);
+      }
+    }, 700);
+  }, [loadingText, text]);
+
+  return (
+    <div className="h-screen flex items-center justify-center backdrop-blur-sm bg-black/30 fixed inset-0">
+      <div className="p-5 w-3/4 max-w-100 h-84 bg-white rounded-2xl shadow flex flex-col gap-5 items-center align-midle justify-center">
+        <BeatLoader color={"#d4d4d4"} size={30} aria-label="Loading" />
+        <p>{loadingText}</p>
+      </div>
+    </div>
+  );
+}
