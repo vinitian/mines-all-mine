ce9b75b29376a650bd399abaa1db7abf9c32b1c1
sasirasys <131147291+Sasirasys@users.noreply.github.com>
2025-10-10 23:47:27 +0700

    fix: can't connect via ip + socket.js wrong param

--- socket.js
+++ socket.js
@@ -1,11 +1,13 @@
-import { io } from 'socket.io-client';
+"use client";
 
-const socket = io('http://localhost:3000', {
-    autoConnect: true,  // Set to true to auto-connect
-    reconnection: true,
-    reconnectionDelay: 1000,
-    reconnectionDelayMax: 5000,
-    reconnectionAttempts: 5
+import { io } from "socket.io-client";
+
+const socket = io({
+  autoConnect: true, // Set to true to auto-connect
+  reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  reconnectionAttempts: 5,
 });
 
 export default socket;
