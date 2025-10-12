741dc09d55a40339fdef1720be1c815a820d22eb
sasirasys <131147291+Sasirasys@users.noreply.github.com>
2025-10-12 11:50:50 +0700

    feat: basic lobby layout

--- app/lobby/[id]/page.tsx
+++ app/lobby/[id]/page.tsx
@@ -1,5 +1,21 @@
+import RoomSettings from "@/app/roomSetting";
 import RoomSetting from "@/app/roomSetting";
+import Chat from "@/components/Chat";
+import RoomName from "@/components/RoomName";
 
 export default function Page() {
-  return;
+  return (
+    <div>
+      <RoomName roomName="Vinitian's Room" roomCode={1234} />
+      <div className="flex">
+        <div className="flex flex-col">
+          <div className="bg-gray-dark">
+            Player 6/8 sadfasf asddads asdfasdf
+          </div>
+          <Chat />
+        </div>
+        <RoomSettings />
+      </div>
+    </div>
+  );
 }
