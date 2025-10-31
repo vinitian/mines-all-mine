851e1c27ba00ae61dda97e48437a4c0f892251b9
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2025-10-31 09:26:46 +0700

    refactor: delete unused methods

--- components/RoomSetting.tsx
+++ components/RoomSetting.tsx
@@ -96,37 +96,6 @@ export default function RoomSettings({
     };
   }, [router]);
 
-  const handleEditRoom = async (state) => {
-    const bombs = densityToCount(bombCount, mapSize);
-    // update room settings in database
-    try {
-      const response = await editRoom({
-        user_id: socket.auth.userID,
-        name: roomname,
-        size: mapSize,
-        bomb_count: bombs,
-        turn_limit: turnLimit,
-        player_limit: playerLimit,
-        chat_enabled: chatState,
-      });
-      const newRoomSettings = {
-        name: roomname,
-        size: mapSize,
-        bomb_density: bombCount,
-        timer: turnLimit,
-        player_limit: playerLimit,
-        chat_enabled: chatState,
-      };
-      // emit setting update to server
-      socket.emit("room:settings-updated", {
-        roomID: room.id,
-        settings: newRoomSettings,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
   // Send default state to server
   useEffect(() => {
     if (isHost) {
@@ -143,18 +112,6 @@ export default function RoomSettings({
       );
     }
   }, []);
-
-  // useEffect(() => {
-  //   if (firstUpdate) {
-  //     setFirstUpdate(false);
-  //     return;
-  //   }
-  //   if (isHost) {
-  //     console.log("182-calling handleEditRoom()..");
-  //     handleEditRoom();
-  //   }
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [mapSize, bombCount, turnLimit, playerLimit, chatState]);
 
   const handleStartGame = async () => {
     if (!mapSize) return;
@@ -427,30 +384,6 @@ export default function RoomSettings({
               bombCount: densityToCount(bombCount, mapSize),
               turnLimit,
             });
-
-            // socket.emit(
-            //   "settings:update",
-            //   {
-            //     size: mapSize,
-            //     bombCount: bombs,
-            //     turnLimit,
-            //     playerLimit,
-            //     chatEnabled: chatState,
-            //     roomName: roomname,
-            //   },
-            //   (ack: any) => {
-            //     if (!ack?.ok) {
-            //       console.error("Failed to save settings:", ack?.error);
-            //       return;
-            //     }
-
-            //     socket.emit("startGame", {
-            //       size: mapSize,
-            //       bombCount: densityToCount(bombCount, mapSize),
-            //       turnLimit,
-            //     });
-            //   }
-            // );
           }}
         />
       </div>
